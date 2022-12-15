658a974b3befe3fc95f854c5974c2d424c48ac0b
gondr99 <gondr99@gmail.com>
2022-12-15 10:19:11 +0900

    add enemy spawn animation

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -13,6 +13,9 @@ var startDirection: Vector2 = Vector2.RIGHT
 #Layer는 해당 물체의 피직스가 어느 레이어 있을 것인가를 체크하는것이고
 #Mask는 해당 물체가 어느 레이어에 있는 애랑 충돌을 체크할 것인가 에 대한 문제이다.
 
+export(bool) var isSpawning:bool = true
+
+
 func _ready():
 	#direction = Vector2.RIGHT if startDirection == Direction.RIGHT else Vector2.LEFT
 	direction = startDirection
@@ -20,6 +23,9 @@ func _ready():
 	$HitBoxArea2D.connect("area_entered", self, "on_hitbox_entered")
 
 func _process(delta):
+	if isSpawning : 
+		return
+		
 	velocity.x = (direction * maxSpeed).x
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -28,7 +34,7 @@ func _process(delta):
 	velocity.y += gravity * delta
 	
 	#방향이 오른쪽이면 가로 플립
-	$AnimatedSprite.flip_h = true if direction.x > 0 else false
+	$Visuals/AnimatedSprite.flip_h = true if direction.x > 0 else false
 	
 func on_goal_entered(_other):
 	direction *= -1 #플립
